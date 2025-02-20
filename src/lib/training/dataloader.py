758ea5dacbae6fc9b58c1bb906d65911d89b2649
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-02-20 22:04:55 +0100

    fix: update batch size and print device

--- src/lib/training/dataloader.py
+++ src/lib/training/dataloader.py
@@ -2,7 +2,7 @@ from torch.utils.data import DataLoader
 
 
 def dataloader(train_dataset, val_dataset, test_dataset):
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
     return train_dataloader, val_dataloader, test_dataloader
