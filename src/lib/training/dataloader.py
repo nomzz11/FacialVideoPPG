83bf99dcd783ca48260d24a28688af325c7d65a0
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-02-20 22:49:55 +0100

    fix: ensure batch size is 64

--- src/lib/training/dataloader.py
+++ src/lib/training/dataloader.py
@@ -5,4 +5,5 @@ def dataloader(train_dataset, val_dataset, test_dataset):
     train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    print(f"Batch size utilisé : {64}")
     return train_dataloader, val_dataloader, test_dataloader
