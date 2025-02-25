11f487dfe6e69a65a16ae6b21f5ac0d5e96bd7ec
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-02-25 17:02:13 +0100

    perf: freeze ResNet layers and updating classifier

--- src/lib/training/resnet.py
+++ src/lib/training/resnet.py
@@ -5,8 +5,14 @@ import torchvision.models as models
 class ResNetPPG(nn.Module):
     def __init__(self):
         super(ResNetPPG, self).__init__()
-        self.resnet = models.resnet50(pretrained=True)
-        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 1)
+        self.resnet = models.resnet152(pretrained=True)
+
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+
+        self.resnet.fc = nn.Sequential(
+            nn.Linear(self.resnet.fc.in_features, 256), nn.ReLU(), nn.Linear(256, 1)
+        )
 
     def forward(self, x):
         return self.resnet(x)
