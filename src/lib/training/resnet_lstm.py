2cdd1253e7964d53c55a290a816baba0fcb9c35b
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-02-26 15:48:52 +0100

    feat: integration of ResNet + LSTM for the management of sequence temporality

---
+++ src/lib/training/resnet_lstm.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class ResNetLSTM(nn.Module):
+    def __init__(self, hidden_size=128, num_layers=2, sequence_length=30):
+        super(ResNetLSTM, self).__init__()
+
+        self.resnet = models.resnet152(pretrained=True)
+
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+
+        self.feature_size = self.resnet.fc.in_features
+
+        self.resnet.fc = nn.Identity()
+
+        self.lstm = nn.LSTM(
+            input_size=self.feature_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+        )
+
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        batch_size, seq_len, C, H, W = x.shape
+        x = x.view(batch_size * seq_len, C, H, W)
+
+        features = self.resnet(x)  # (batch_size * seq_len, feature_size)
+        features = features.view(batch_size, seq_len, -1)
+
+        last_out, _ = self.lstm(features)
+
+        last_out = self.fc(last_out[:, -1, :])
+
+        return last_out
--- src/lib/training/split_dataset.py
+++ src/lib/training/split_dataset.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 from .dataset import FacialVideoDataset
 
 
-def split_dataset(dataset, split_strategy):
+def split_dataset(dataset, split_strategy, use_lstm=False):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = os.path.join(base_dir, "../../../refined_data")
     transform = transforms.Compose(
@@ -14,13 +14,25 @@ def split_dataset(dataset, split_strategy):
     )
 
     train_dataset = dataset(
-        data_dir, split="train", split_strategy=split_strategy, transform=transform
+        data_dir,
+        split="train",
+        split_strategy=split_strategy,
+        transform=transform,
+        use_lstm=use_lstm,
     )
     val_dataset = dataset(
-        data_dir, split="val", split_strategy=split_strategy, transform=transform
+        data_dir,
+        split="val",
+        split_strategy=split_strategy,
+        transform=transform,
+        use_lstm=use_lstm,
     )
     test_dataset = dataset(
-        data_dir, split="test", split_strategy=split_strategy, transform=transform
+        data_dir,
+        split="test",
+        split_strategy=split_strategy,
+        transform=transform,
+        use_lstm=use_lstm,
     )
 
     return train_dataset, val_dataset, test_dataset
