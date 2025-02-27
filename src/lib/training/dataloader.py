45e22ad8214359f6020b7af2a4537d774a8cfbf9
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-02-27 12:23:49 +0100

    feat: add --batch_size argument for command-line configuration

--- src/lib/training/dataloader.py
+++ src/lib/training/dataloader.py
@@ -1,9 +1,9 @@
 from torch.utils.data import DataLoader
 
 
-def dataloader(train_dataset, val_dataset, test_dataset):
-    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
-    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+def dataloader(train_dataset, val_dataset, test_dataset, batch_size):
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     print(f"Batch size utilisé : {64}")
     return train_dataloader, val_dataloader, test_dataloader
