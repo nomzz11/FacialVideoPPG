56900e7c8249c9d036dd646e60da648a069e8669
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-02-28 16:00:47 +0100

    refactor: defreeze ResNet layers

--- src/lib/training/resnet.py
+++ src/lib/training/resnet.py
@@ -7,8 +7,10 @@ class ResNetPPG(nn.Module):
         super(ResNetPPG, self).__init__()
         self.resnet = models.resnet152(pretrained=True)
 
+        """
         for param in self.resnet.parameters():
             param.requires_grad = False
+        """
 
         self.resnet.fc = nn.Sequential(
             nn.Linear(self.resnet.fc.in_features, 256), nn.ReLU(), nn.Linear(256, 1)
