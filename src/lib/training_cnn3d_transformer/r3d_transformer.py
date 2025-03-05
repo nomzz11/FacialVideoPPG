847d63e25eea71cb6c428be07c942bfd4689e4bd
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-03-05 17:55:54 +0100

    fix: reshape r3d output to make it compatible with the transformer

--- src/lib/training_cnn3d_transformer/r3d_transformer.py
+++ src/lib/training_cnn3d_transformer/r3d_transformer.py
@@ -18,8 +18,6 @@ class r3d_transformer(nn.Module):
 
     def forward(self, x):
         x = self.backbone(x)
-        print(x.shape)
-        x = x.view(x.size(2), x.size(0), x.size(1))
         src, tgt = x, x
         x = self.transformer(src, tgt)
         x = self.fc(x)
