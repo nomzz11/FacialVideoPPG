705683caa77f2750faf321ec013b20aa422ae983
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-03-05 11:04:25 +0100

    fix: disable shuffle to preserve temporal order of videos

--- src/lib/training/dataloader.py
+++ src/lib/training/dataloader.py
@@ -2,7 +2,7 @@ from torch.utils.data import DataLoader
 
 
 def dataloader(train_dataset, val_dataset, test_dataset, batch_size):
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     return train_dataloader, val_dataloader, test_dataloader
