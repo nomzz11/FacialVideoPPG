723fdb5241f18aa0527f4e342d37d71d2d3270f8
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-03-05 16:01:42 +0100

    feat: add R3D + Transformer model

---
+++ src/lib/training_cnn3d_transformer/r3d_transformer.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from torchvision.models.video import r3d_18
+from torch.nn import Transformer
+
+
+class r3d_transformer(nn.Module):
+    def __init__(self):
+        super(r3d_transformer, self).__init__()
+        # Load pre-trained ResNet3D-18 model on videos to extract features
+        self.backbone = r3d_18(pretrained=True)
+
+        # Transformer to capture temporal relationships
+        self.transformer = Transformer(d_model=512, nhead=8, num_encoder_layers=6)
+
+        # FC layer to predict final PPG value
+        self.fc = nn.Linear(512, 1)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.transformer(x)
+        x = self.fc(x)
+        return x
