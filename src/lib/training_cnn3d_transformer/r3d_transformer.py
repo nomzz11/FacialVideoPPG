d758e9e757c057deb1628f38f6b65953c521959b
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-03-07 16:26:23 +0100

    feat: add spatial attention after r3d

--- src/lib/training_cnn3d_transformer/r3d_transformer.py
+++ src/lib/training_cnn3d_transformer/r3d_transformer.py
@@ -4,6 +4,29 @@ from torchvision.models.video import r3d_18
 from torch.nn import Transformer
 
 
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        # Convolution sur la concaténation des deux cartes attention (max et moyenne)
+        self.conv1 = nn.Conv3d(2, 1, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Calcul du max et du moyen sur les canaux (C) de la dimension spatiale
+        avg_out = torch.mean(x, dim=1, keepdim=True)  # Moyenne sur les canaux
+        max_out, _ = torch.max(x, dim=1, keepdim=True)  # Max sur les canaux
+
+        # Concaténation des deux sorties (moyenne et max) pour capturer plus d'informations
+        out = torch.cat([avg_out, max_out], dim=1)
+
+        # Application de la convolution et de la fonction sigmoïde
+        out = self.conv1(out)
+        out = self.sigmoid(out)
+
+        # Retourner l'entrée modifiée avec l'attention spatiale
+        return x * out
+
+
 class r3d_transformer(nn.Module):
     def __init__(self):
         super(r3d_transformer, self).__init__()
@@ -15,8 +38,8 @@ class r3d_transformer(nn.Module):
 
         self.backbone.fc = nn.Identity()
 
-        # Transformer to capture temporal relationships
-        self.transformer = Transformer(d_model=512, nhead=8, num_encoder_layers=6)
+        # Ajouter l'attention spatiale
+        self.spatial_attention = SpatialAttention(kernel_size=7)
 
         # FC layer to predict final PPG value
         self.fc = nn.Linear(512, 1)
@@ -24,8 +47,8 @@ class r3d_transformer(nn.Module):
     def forward(self, x):
         x = self.backbone(x)
         print(x.shape)
-        src, tgt = x, x
-        x = self.transformer(src, tgt)
+        x = self.spatial_attention(x)
         x = self.fc(x)
+        print(x.shape)
         x = x.squeeze(-1)
         return x
