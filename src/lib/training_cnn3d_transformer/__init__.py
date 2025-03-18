1e7294265c6bbfadddbbae75fc37a4b051d08440
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-03-18 12:48:41 +0100

    feat: add pearson loss in r3d model

--- src/lib/training_cnn3d_transformer/__init__.py
+++ src/lib/training_cnn3d_transformer/__init__.py
@@ -2,3 +2,4 @@ from .dataset import FacialVideoDataset
 from .dataloader import dataloader
 from .split_dataset import split_dataset
 from .r3d_transformer import r3d_transformer
+from .pearson_loss import PearsonLoss
