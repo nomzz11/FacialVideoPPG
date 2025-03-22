f08d0244d1d6cb465871fe3a9b6556882984ba1c
nomzz11 <110392413+nomzz11@users.noreply.github.com>
2025-03-22 12:34:00 +0100

    chore: remove in_order and num_workers arguments in dataloader

--- src/lib/train_r3d_attention_data_mtcnn/dataloader.py
+++ src/lib/train_r3d_attention_data_mtcnn/dataloader.py
@@ -2,13 +2,7 @@ from torch.utils.data import DataLoader
 
 
 def dataloader(train_dataset, val_dataset, test_dataset, batch_size):
-    train_dataloader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        in_order=False,
-        num_workers=1,
-    )
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     return train_dataloader, val_dataloader, test_dataloader
